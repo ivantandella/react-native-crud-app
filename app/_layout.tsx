739617c29af504804b8c220d4753f8a230e19cd5
Ivan Tandella <ivantandella2@gmail.com>
2025-01-03 15:04:01 +0700

    add crud

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,7 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 export default function RootLayout() {
   return (
     <SafeAreaProvider>
-      <Stack></Stack>
+      <Stack screenOptions={{ headerShown: false }} />
     </SafeAreaProvider>
   );
 }
